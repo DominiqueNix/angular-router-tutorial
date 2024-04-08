5272809dc26fc735887e8d55cc19503a66261815
DominiqueNix <dominiquenix@google.com>
2024-04-08 11:30:37 -0500

    followed angular routing tutorial

---
+++ angular-router-sample/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    RouterOutlet, 
+    RouterLink
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
